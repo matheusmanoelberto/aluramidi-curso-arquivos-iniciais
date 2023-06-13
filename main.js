78d222a81ffbb2866d0eb15a75b2c0a472f1ff8e
matheusmanoelberto <matheusmanoel51@gmail.com>
2023-06-13 19:07:58 -0300

    atualizacao 13/06

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();  
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();  
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while(contador < listaDeTeclas.length){
+    listaDeTeclas[contador].onclick = tocaSom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
