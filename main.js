2948b4350d14cd68a44ba885f8511ad7dfea41b2
matheusmanoelberto <matheusmanoel51@gmail.com>
2023-06-14 17:43:20 -0300

    bugs elemento

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();  
+function tocaSom(seletorAudio){
+    
+   const elemento = document.querySelector(seletorAudio); 
+   
+   if(elemento != null  && elemento.localNome === 'audio') {
+        elemento.play();
+   }
+   else{
+        console.log('Elemento não encontrado ou seletor invalido');
+   }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
